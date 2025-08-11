c5e6b7a1e6dd7df045e5563de51958131206e274
aish <aish@10.0.0.62>
2025-08-11 18:17:30 -0400

    commit parent class

---
+++ src/JavaBasics/Parent.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+public class Parent {
+	
+	public void provideShelter(){
+		System.out.println("My parents own a house");
+	}
+	
+	public void prepareFood(){
+		System.out.println("My parents cook food");
+	}
+	
+	public void doJob(){
+		System.out.println("My parents goes to office");	
+	}
+	
+	public void payFee(){
+		System.out.println("My parents pay my school fees");	
+
+}
+	
+}
